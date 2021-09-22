7d94febd951b8a9937681eed14fa4faed7828f04
Lucas Perna <lucasmalamao@gmail.com>
2021-09-22 19:48:31 -0300

    client backend

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas', '47856236987', 5000.0, '1990-12-30T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João', '47850006987', 8000.0, '1998-10-25T10:30:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pedro', '00859996987', 5000.5, '1996-07-10T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fabio', '47865836514', 3500.0, '1994-01-09T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana', '47832535584', 4000.0, '1994-03-08T10:30:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Isabela', '47856698523', 10000.0, '1980-06-17T10:30:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Daniele', '47856233254', 5900.0, '1999-06-28T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Guilherme', '47859584632', 8800.0, '1994-10-06T10:30:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Dair', '00265415936', 6500.0, '1993-08-09T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria', '47856924987', 5000.0, '1992-11-15T10:30:00Z', 1);
